f9323b0b07d4932004c3f6250e2af244c647f044
Jonathan Carrillo <joncak@volvocars.com>
2024-02-02 06:25:03 +0100

    add support for k8s tags

--- modules/terraform-aws-vpc/main.tf
+++ modules/terraform-aws-vpc/main.tf
@@ -3,20 +3,21 @@ resource "aws_vpc" "self" {
   cidr_block           = var.cidr_block
   instance_tenancy     = "default"
   enable_dns_hostnames = true
-
+  enable_dns_support   = true
   tags = {
     Name = "${var.name_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.self.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.azs[count.index]
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.self.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
 
   tags = {
-    Name = "${var.name_prefix}-public-${count.index}"
+    Name                     = "${var.name_prefix}-public-${count.index}"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
@@ -27,7 +28,8 @@ resource "aws_subnet" "private" {
   availability_zone = var.azs[count.index]
 
   tags = {
-    Name = "${var.name_prefix}-private-${count.index}"
+    Name                              = "${var.name_prefix}-private-${count.index}"
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
 
@@ -85,7 +87,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.self.id
+    nat_gateway_id = aws_nat_gateway.self[0].id
   }
   tags = {
     Name = "${var.name_prefix}-private-route-table"
